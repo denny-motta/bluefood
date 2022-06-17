6780b8e272c15d009e020eab3d21245a1cb9f9e4
Denilson <contato.deninunes@gmail.com>
2022-06-16 23:47:30 -0300

    initial commit

---
+++ src/main/java/br/com/softblue/bluefood/BluefoodProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluefoodProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluefoodProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/softblue/bluefood/domain/pedido/RelatorioItemFaturamento.java
@@ -0,0 +1,19 @@
+package br.com.softblue.bluefood.domain.pedido;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RelatorioItemFaturamento {
+
+	private String nome;
+	
+	private Long quantidade;
+	
+	private BigDecimal valor;
+	
+	
+}
